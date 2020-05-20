85e5f229b30f6159a25d5673619b05e857543dff
yueaptx <yueaptx@ucla.edu>
2020-05-19 22:52:15 -0700

    Switch on and off App

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lightSwitchLbl: UILabel!
+    
+    var lightsAreOn = false
+    
+    
+    
+    @IBAction func lightSwitchBtnClicked(_ sender: Any) {
+        if lightsAreOn == true{
+            // turn them off.
+            lightSwitchLbl.text = "Lights Off"
+            lightSwitchLbl.textColor = UIColor.white
+            view.backgroundColor = UIColor.black
+            lightsAreOn.toggle()
+        } else {
+            // turn them on.
+            lightSwitchLbl.text = "Lights On"
+            lightSwitchLbl.textColor = UIColor.black
+            view.backgroundColor = UIColor.white
+            lightsAreOn.toggle()
+        }
+
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
