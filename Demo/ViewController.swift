e606a872d2fa79b316cfcdc00d61bda213af46ef
yueaptx <yueaptx@ucla.edu>
2020-05-21 22:53:17 -0700

    First LLan

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,36 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var lightSwitchLbl: UILabel!
     
-    var lightsAreOn = false
-    
-    
-    
-    @IBAction func lightSwitchBtnClicked(_ sender: Any) {
-        if lightsAreOn == true{
-            // turn them off.
-            lightSwitchLbl.text = "Lights Off"
-            lightSwitchLbl.textColor = UIColor.white
-            view.backgroundColor = UIColor.black
-            lightsAreOn.toggle()
-        } else {
-            // turn them on.
-            lightSwitchLbl.text = "Lights On"
-            lightSwitchLbl.textColor = UIColor.black
-            view.backgroundColor = UIColor.white
-            lightsAreOn.toggle()
-        }
-
-    }
-    
+    @IBOutlet weak var LLanFightLbl: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        LLanFightLbl.isHidden = true
     }
 
 
+    @IBAction func LLanFightBtn(_ sender: Any) {
+        LLanFightLbl.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.LLanFightLbl.isHidden = true
+        }
+    }
+    
+    
+    
+    //    @IBAction func lightSwitchBtnClicked(_ sender: Any) {
+    //        if lightsAreOn == true{
+    //            // turn them off.
+    //            lightSwitchLbl.text = "Lights Off"
+    //            lightSwitchLbl.textColor = UIColor.white
+    //            view.backgroundColor = UIColor.black
+    //            lightsAreOn.toggle()
+    //        } else {
+    //            // turn them on.
+    //            lightSwitchLbl.text = "Lights On"
+    //            lightSwitchLbl.textColor = UIColor.black
+    //            view.backgroundColor = UIColor.white
+    //            lightsAreOn.toggle()
+    //        }
+
+    //    }
 }
 
